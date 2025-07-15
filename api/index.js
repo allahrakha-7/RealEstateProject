9e3f93adcea1a6f11ba453c548e1e78c55069fe0
Allah Rakha <armozi07911000@gmail.com>
2025-07-15 22:29:43 +0500

    Add and Create listing api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js'
 
 const app = express();
 
@@ -24,6 +24,7 @@ mongoose.connect(process.env.MONGO)
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
