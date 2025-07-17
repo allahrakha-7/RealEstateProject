d8ce4305b96400b62a09a0b94fa6890cce8c9d0d
Allah Rakha <armozi07911000@gmail.com>
2025-07-17 19:33:00 +0500

    Add pre installation configuration

--- api/index.js
+++ api/index.js
@@ -7,11 +7,6 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -29,12 +24,6 @@ mongoose.connect(process.env.MONGO)
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import lineClamp from '@tailwindcss/line-clamp'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +14,6 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
-    lineClamp,
     react(),
   ],
 })
