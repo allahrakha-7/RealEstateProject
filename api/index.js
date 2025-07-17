bf2c6e19b374139ad7fd08ca8f0dca26f5e311a9
Allah Rakha <armozi07911000@gmail.com>
2025-07-17 18:39:18 +0500

    Add Pre installation configuration

--- api/index.js
+++ api/index.js
@@ -1,29 +1,30 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
+import listingRouter from './routes/listing.route.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const _dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-// eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGO)
-.then(() => {
+  .then(() => {
     console.log('Connected to MongoDB!');
-})
-.catch(err => {
+  })
+  .catch(err => {
     console.error('Failed to connect to MongoDB:', err.message);
-});
+  });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
@@ -32,21 +33,20 @@ app.use('/api/listing', listingRouter);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  console.log('Server is running on port 3000!');
 });
 
 
-// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
