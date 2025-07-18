0853c503eef5ae73369327b9537219620b809d8b
Allah Rakha <armozi07911000@gmail.com>
2025-07-18 12:55:58 +0500

    fix issuse to live

--- api/index.js
+++ api/index.js
@@ -17,34 +17,23 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-const __dirname = path.resolve();
-
+  
 const app = express();
+
+app.use(cors({
+  origin: ['https://real-estate-web-app-zeta.vercel.app'],
+  credentials: true
+}));
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-app.use(cors({
-  origin: ['https://real-estate-project-chi-five.vercel.app'],
-  credentials: true
-}));
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -55,3 +44,10 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+}
